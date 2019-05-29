010e19bddf7fd1c27f5148f56d9f5b74254b0792
just-only <2098844398@qq.com>
2019-05-29 13:56:57 +0800

    first

--- src/test/java/com/job/Main.java
+++ src/test/java/com/job/Main.java
@@ -2,9 +2,9 @@ package com.job;
 
 public class Main {
 	public static void main(String[] args) {
-		LoginTicketTests loginTicketTests = new LoginTicketTests();
-		loginTicketTests.contextLoads();
-		//MessageTests tests = new MessageTests();
-		//tests.contextLoads();
+		//LoginTicketTests loginTicketTests = new LoginTicketTests();
+		//loginTicketTests.contextLoads();
+		MessageTests tests = new MessageTests();
+		tests.contextLoads();
 	}
 }
