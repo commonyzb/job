b86c5d6e059bb07da38527a5fa03e09ffb879288
xu.jiang <2225144254@qq.com>
2019-05-30 18:32:50 +0800

    Subtype Supertype

--- src/main/java/com/job/serviceImpl/SubtypeServiceImpl.java
+++ src/main/java/com/job/serviceImpl/SubtypeServiceImpl.java
@@ -1,8 +1,42 @@
 package com.job.serviceImpl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.job.dao.SubtypeDAO;
+import com.job.model.Subtype;
+import com.job.service.SubtypeService;
+
 @Service
-public class SubtypeServiceImpl {
+public class SubtypeServiceImpl implements SubtypeService{
+
+	@Resource
+	SubtypeDAO subtypedao;
+	@Override
+	public int addSubtype(Subtype subtype) {
+		// TODO Auto-generated method stub
+		return this.subtypedao.addSubtype(subtype);
+	}
+
+	@Override
+	public int setSubtype(int id, String typename) {
+		// TODO Auto-generated method stub
+		return this.subtypedao.setSubtype(id, typename);
+	}
+
+	@Override
+	public Subtype getSubtype(int id) {
+		// TODO Auto-generated method stub
+		return this.subtypedao.getSubtype(id);
+	}
+
+	@Override
+	public List<Subtype> getAllSubtype() {
+		// TODO Auto-generated method stub
+		return this.subtypedao.getAll();
+	}
 
 }
